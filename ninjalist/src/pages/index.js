6d560defd1e3fc7efaab433e058339b26324913e
mbaranturkmen <kilexey@gmail.com>
2023-02-24 17:01:23 +0000

    finish nextjs lesson 3 adding other components

---
+++ ninjalist/src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1>homwpage</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod luctus purus, sed feugiat nisl finibus id. Suspendisse porta lacus elit, quis aliquam sem hendrerit eget. Duis a mattis lectus, non dapibus est. Aliquam enim diam, ornare ut consequat ac, viverra vitae nulla. In tincidunt ligula quam, eget sodales eros sollicitudin in. Nullam hendrerit elit vel est lobortis, a euismod ligula venenatis. Cras tristique pulvinar nulla, nec aliquam urna vestibulum in. Sed egestas mauris et tempor dictum. In varius nibh urna. Nulla quam lorem, dictum et venenatis eu, sodales vel quam.</p>
+      <Footer />
+    </>
+  )
+}
---
+++ ninjalist/src/pages/ninjas/test.js
@@ -0,0 +1,9 @@
+const Test = () => {
+  return ( 
+    <p>
+      hi
+    </p>
+   );
+}
+ 
+export default Test;

a1e0bc42ee52ce4a5c472e92e627ce2241daba9f
mbaranturkmen <kilexey@gmail.com>
2023-02-25 14:58:25 +0000

    finish lesson 10 fetching data w/ `getStatisProps`

--- ninjalist/src/pages/ninjas/index.js
+++ ninjalist/src/pages/ninjas/index.js
@@ -1,7 +1,28 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css';
+
+// runs before the component is rendered
+// return props object
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data } // will be passed to the page component as props
+  }
+}
+
+const Ninjas = (props) => {
   return ( 
     <div>
       <h1>All ninjas</h1>
+      {props.ninjas.map(ninja => (
+        <div key={ ninja.id }>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </div>
+        )
+        )}
     </div>
    );
 }

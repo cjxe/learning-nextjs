2dd38669bb02f15c43386868740a0793a3c0ea22
mbaranturkmen <kilexey@gmail.com>
2023-02-25 11:10:05 +0000

    finish lesson 6 adding CSS styles

--- ninjalist/src/components/Footer.js
+++ ninjalist/src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return ( 
-    <div>
+    <footer>
       <p>Copyright 2023</p>
-    </div>
+    </footer>
    );
 }
  
--- ninjalist/src/pages/index.js
+++ ninjalist/src/pages/index.js
@@ -10,10 +10,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1>homwpage</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod luctus purus, sed feugiat nisl finibus id. Suspendisse porta lacus elit, quis aliquam sem hendrerit eget. Duis a mattis lectus, non dapibus est. Aliquam enim diam, ornare ut consequat ac, viverra vitae nulla. In tincidunt ligula quam, eget sodales eros sollicitudin in. Nullam hendrerit elit vel est lobortis, a euismod ligula venenatis. Cras tristique pulvinar nulla, nec aliquam urna vestibulum in. Sed egestas mauris et tempor dictum. In varius nibh urna. Nulla quam lorem, dictum et venenatis eu, sodales vel quam.</p>
-      <Link href="/ninjas">See Ninja Listing</Link>
-    </>
+    <div>
+      <h1 className={styles.title}>homwpage</h1>
+      <p  className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod luctus purus, sed feugiat nisl finibus id. Suspendisse porta lacus elit, quis aliquam sem hendrerit eget. Duis a mattis lectus, non dapibus est. Aliquam enim diam, ornare ut consequat ac, viverra vitae nulla. In tincidunt ligula quam, eget sodales eros sollicitudin in. Nullam hendrerit elit vel est lobortis, a euismod ligula venenatis. Cras tristique pulvinar nulla, nec aliquam urna vestibulum in. Sed egestas mauris et tempor dictum. In varius nibh urna. Nulla quam lorem, dictum et venenatis eu, sodales vel quam.</p>
+      <Link href="/ninjas" className={styles.btn}>See Ninja Listing</Link>
+    </div>
   )
 }

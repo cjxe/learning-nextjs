18adf1580c30f8e27bb0571f39ebecd5ed1f7f37
mbaranturkmen <kilexey@gmail.com>
2023-02-25 14:41:06 +0000

    finish lesson 9 add images and metadata

--- ninjalist/src/pages/about.js
+++ ninjalist/src/pages/about.js
@@ -1,10 +1,19 @@
+
+import Head from 'next/head';
+
 const About = () => {
   return (  
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod luctus purus, sed feugiat nisl finibus id. Suspendisse porta lacus elit, quis aliquam sem hendrerit eget. Duis a mattis lectus, non dapibus est. Aliquam enim diam, ornare ut consequat ac, viverra vitae nulla. In tincidunt ligula quam, eget sodales eros sollicitudin in. Nullam hendrerit elit vel est lobortis, a euismod ligula venenatis. Cras tristique pulvinar nulla, nec aliquam urna vestibulum in. Sed egestas mauris et tempor dictum. In varius nibh urna. Nulla quam lorem, dictum et venenatis eu, sodales vel quam.</p>
-      
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" contenet="ninjas"></meta>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod luctus purus, sed feugiat nisl finibus id. Suspendisse porta lacus elit, quis aliquam sem hendrerit eget. Duis a mattis lectus, non dapibus est. Aliquam enim diam, ornare ut consequat ac, viverra vitae nulla. In tincidunt ligula quam, eget sodales eros sollicitudin in. Nullam hendrerit elit vel est lobortis, a euismod ligula venenatis. Cras tristique pulvinar nulla, nec aliquam urna vestibulum in. Sed egestas mauris et tempor dictum. In varius nibh urna. Nulla quam lorem, dictum et venenatis eu, sodales vel quam.</p>
+        
+      </div>
+    </>
   );
 }
  

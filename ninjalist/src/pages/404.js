0f43e9a067c47485502113b04f3b6d5e7d55515c
mbaranturkmen <kilexey@gmail.com>
2023-02-25 12:33:13 +0000

    finish lesson 8 redirecting users

--- ninjalist/src/pages/404.js
+++ ninjalist/src/pages/404.js
@@ -1,7 +1,20 @@
 import Link from "next/link";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+
 
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // redirect the user after 3 seconds
+    setTimeout(() => {
+      router.push('/')
+    }, 3000)
+    console.log('abc');
+  }, [])
   return ( 
     <div className="not-found">
       <h1>Oooops...</h1>
